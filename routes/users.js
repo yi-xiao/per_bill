88cb6ed2c64fa4acb0de383915e09e2b5af0a58b
xiao-yi <1585480284@qq.com>
2018-12-13 14:52:18 +0800

    update: 创建用户与修改用户信息接口

--- routes/users.js
+++ routes/users.js
@@ -12,12 +12,47 @@ router.get('/', async (req, res, next) => {
 
 /* create user */
 router.post('/createUser', async (req, res, next) => {
-	let { name,nickName='',sex=0,age=0 } = req.body;
-    let sql = `insert into user(name, nickName, age, sex) values('${name}',"${nickName}",${sex},${age})`;
+	let { name,nickName='',sex=0,age=0,avatar='' } = req.body;
+    // let sql = `insert into user(name, nickName, age, sex) values('${name}',"${nickName}",${sex},${age})`;
     			 
-    console.log(sql)
-    let result = await db.query(sql);
-     res.send(result);
+    // console.log(sql)
+    // let result = await db.query(sql);
+    let options = []
+    let result = await userModel.create({
+        name,
+        nickName: nickName ? nickName : null,
+        sex,
+        age,
+        avatar: avatar ? avatar : null
+    })
+    res.send(result);
+});
+
+
+
+/* update user */
+router.post('/updateUser', async (req, res, next) => {
+    let { name,nickName,sex,age,avatar, user_id } = req.body;
+
+    let options = {}
+    if(name !== null && !name){
+        res.send('姓名不能为空');
+        res.end();
+    }else{
+        options.name = name;
+        if(nickName !== null) options.nickName = nickName;
+        if(sex !== null) options.sex = sex;
+        if(age !== null) options.age = age;
+        if(avatar !== null) options.avatar = avatar;
+
+        let result = await userModel.update(options,{
+            where: {
+                id: user_id
+            }
+        })
+        res.send(result); 
+    }
+    
 });
 
 module.exports = router;
